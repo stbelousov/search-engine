d4d61d75ddbebbcebd0667dc640a29bd7775da8f
Stepan Belousov <int128t@gmail.com>
2020-06-24 20:02:24 +1200

    Remove redundant comments

--- src/searcher.cpp
+++ src/searcher.cpp
@@ -16,8 +16,6 @@ int main(int argc, char* argv[]) {
     Index index(index_file);
 
     // Process queries
-    // There will be only one query per input, as far as I understood from the task description
-    // But this program also can process multiple queries line by line, just in case
     std::string query;
     while (std::getline(std::cin, query)) {
         index.ProcessQuery(query);
